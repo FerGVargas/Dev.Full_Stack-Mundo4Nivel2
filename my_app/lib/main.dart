8b767f30fa51fa3a7129cf2a14e4599d16ac5469
Fer G. Vargas <fernandadguimaraes@hotmail.com>
2024-05-05 21:05:38 -0300

    Atualizando projeto

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
+/// Classe principal 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,115 +19,198 @@ class MyApp extends StatelessWidget {
   }
 }
 
+/// Página inicial do App
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key});
 
   @override
   Widget build(BuildContext context) {
     Color color = Theme.of(context).primaryColor;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Viajar é mudar a roupa da alma.'),
-      ),
-      body: ListView(
-        children: [
-          Image.asset('images/top.jpg',
-            width: MediaQuery.of(context).size.width,
-            height: 240,
-            fit: BoxFit.cover,
+    return DefaultTabController(
+      length: 4, // Número de guias (links)
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Viajar é mudar a roupa da alma.'),
+          bottom: TabBar(
+            tabs: [
+              Tab(text: 'Destinos'),
+              Tab(text: 'Pacotes de Viagem'),
+              Tab(text: 'Contato'),
+              Tab(text: 'Sobre Nós'),
+            ],
           ),
-          const TitleSection(),
-          ButtonSection(color: color),
-          const TextSection(),
-        ],
-      ),
-    );
-  }
-}
-
-class ButtonSection extends StatelessWidget {
-  final Color color;
-
-  const ButtonSection({Key? key, required this.color}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        _buildButtonColumn(color, Icons.call, 'Call'),
-        _buildButtonColumn(color, Icons.near_me, 'Route'),
-        _buildButtonColumn(color, Icons.share, 'Share'),
-      ],
-    );
-  }
-
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: const TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                showSearch(context: context, delegate: CustomSearchDelegate());
+              },
             ),
+          ],
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Digite seu destino',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  // Conteúdo da guia Destinos
+                  Center(
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'images/top.jpg', // Caminho da imagem
+                          width: MediaQuery.of(context).size.width,
+                          height: 240,
+                          fit: BoxFit.cover,
+                        ),
+                        Text('Seu destino perfeito está aqui!'),
+                      ],
+                    ),
+                  ),
+                  // Conteúdo da guia Pacotes de Viagem
+                  Center(
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'images/pacotes.jpg', // Caminho da imagem
+                          width: MediaQuery.of(context).size.width,
+                          height: 240,
+                          fit: BoxFit.cover,
+                        ),
+                        Text('Temos diversos pacotes disponíveis e também podemos montar um exclusivo.'),
+                      ],
+                    ),
+                  ),
+                  // Conteúdo da guia Contato
+                  Center(
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'images/contato.jpg', //Caminho da imagem 
+                          width: MediaQuery.of(context).size.width,
+                          height: 240,
+                          fit: BoxFit.cover,
+                        ),
+                        Text('Entre em contato conosco.'),
+                      ],
+                    ), 
+                  ),
+                  // Conteúdo da guia Sobre Nós
+                  Center(
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'images/sobre.jpg', // Caminho da imagem
+                          width: MediaQuery.of(context).size.width,
+                          height: 200,
+                          fit: BoxFit.cover,
+                        ),
+                        Text('Sua agência  de viagens para qualquer momento.'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: <Widget>[
+                  Icon(Icons.call),
+                  Text('Ligue'),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Icon(Icons.directions),
+                  Text('Rota'),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Icon(Icons.share),
+                  Text('Compartilhe'),
+                ],
+              ),
+            ],
           ),
         ),
-      ],
-    );
-  }
-}
-
-class TitleSection extends StatelessWidget {
-  const TitleSection({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                Text(
-                  'Dolomitas, Itália',
-                  style: TextStyle(
-                    color: Colors.grey,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          const Icon(
-            Icons.star,
-            color: Colors.yellow,
-          ),
-          const Text('682'),
-        ],
       ),
     );
   }
 }
 
-class TextSection extends StatelessWidget {
-  const TextSection({Key? key}) : super(key: key);
+/// Classe que define o delegado de pesquisa personalizado.
+class CustomSearchDelegate extends SearchDelegate<String> {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
 
   @override
-  Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(32),
-      child: Text(
-        'As Dolomitas, na Itália, garantem alguns dos cenários naturais mais espetaculares do mundo. Não à toa, a famosa cadeia de montanhas nos alpes italianos é considerada Patrimônio Mundial da UNESCO. Mas, afinal, o que fazer nas Dolomitas? Fato é que as Dolomitas mudam muito de acordo com a estação. O cenário branquinho de inverno é lindo, mas é no verão que elas se tornam incomparáveis. Não importa se você vai fazer uma trilha, escalar um montanha ou simplesmente sentar e admirar a paisagem: será inesquecível.',
-        softWrap: true,
-      ),
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, '');
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // Lógica para exibir resultados da pesquisa
+    return Center(
+      child: Text('Resultados para: $query'),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // Sugestões de pesquisa 
+    final List<String> suggestions = [
+      'Destino 1',
+      'Destino 2',
+      'Destino 3',
+      'Destino 4',
+    ];
+
+    final List<String> filteredSuggestions = query.isEmpty
+        ? suggestions
+        : suggestions.where((destino) => destino.toLowerCase().contains(query.toLowerCase())).toList();
+
+    return ListView.builder(
+      itemCount: filteredSuggestions.length,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          title: Text(filteredSuggestions[index]),
+          onTap: () {
+            // Ação ao clicar na sugestão
+            close(context, filteredSuggestions[index]);
+          },
+        );
+      },
     );
   }
 }
